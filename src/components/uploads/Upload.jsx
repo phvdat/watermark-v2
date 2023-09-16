3e41c711de72e43aba2c4371bfa6efb1dca91491
phvdat <phamvandat.hcmut@gmail.com>
2023-09-16 18:16:34 +0700

    add quality

--- src/components/uploads/Upload.jsx
+++ src/components/uploads/Upload.jsx
@@ -10,7 +10,8 @@ function UploadForm() {
 				logoWidth: 1200,
 				logoHeight: 1200,
 				imageWidth: 1200,
-				imageHeight: 1200
+				imageHeight: 1200,
+				quality: 50,
 			}
 		});
 	const [downloadLink, setDownloadLink] = React.useState(null);
@@ -27,6 +28,7 @@ function UploadForm() {
 		formData.append('logoHeight', data.logoHeight);
 		formData.append('imageWidth', data.imageWidth);
 		formData.append('imageHeight', data.imageHeight);
+		formData.append('quality', data.quality);
 
 		try {
 			const response = await axios.post('http://localhost:8000/process', formData);
@@ -46,32 +48,39 @@ function UploadForm() {
 				<div>
 					<label htmlFor="excelFile">Excel File:</label>
 					<input type="file" id="excelFile" {...register('excelFile', { required: true })} />
-					{errors.excelFile && <span>This field is required</span>}
+					{errors.excelFile && <p className='error'>This field is required</p>}
 				</div>
 				<div>
 					<label htmlFor="logoUrl">Logo URL:</label>
-					<input type="text" id="logoUrl" {...register('logoUrl')} />
+					<input type="text" id="logoUrl" {...register('logoUrl', { required: true })} />
+					{errors.logoUrl && <p className='error'>This field is required</p>}
 				</div>
 				<div>
 					<label htmlFor="logoWidth">Logo Width:</label>
 					<input type="number" id="logoWidth" {...register('logoWidth', { min: 0 })} />
-					{errors.logoWidth && <span>Logo width must be a positive number</span>}
+					{errors.logoWidth && <p className='error'>Logo width must be a positive number</p>}
 				</div>
 				<div>
 					<label htmlFor="logoHeight">Logo Height:</label>
 					<input type="number" id="logoHeight" {...register('logoHeight', { min: 0 })} />
-					{errors.logoHeight && <span>Logo height must be a positive number</span>}
+					{errors.logoHeight && <p className='error'>Logo height must be a positive number</p>}
 				</div>
 				<div>
 					<label htmlFor="imageWidth">Image Width:</label>
 					<input type="number" id="imageWidth" {...register('imageWidth', { min: 0 })} />
-					{errors.imageWidth && <span>Image width must be a positive number</span>}
+					{errors.imageWidth && <p className='error'>Image width must be a positive number</p>}
 				</div>
 				<div>
 					<label htmlFor="imageHeight">Image Height:</label>
 					<input type="number" id="imageHeight" {...register('imageHeight', { min: 0 })} />
-					{errors.imageHeight && <span>Image height must be a positive number</span>}
+					{errors.imageHeight && <p className='error'>Image height must be a positive number</p>}
 				</div>
+				<div>
+					<label htmlFor="quality">Quality:</label>
+					<input type="number" id="quality" {...register('quality', { min: 0, max: 100 })} />
+					{errors.quality && <p className='error'>Quality height must be a positive number</p>}
+				</div>
+
 				<button type="submit">Upload and Process</button>
 			</form>
 			{/* Render download link if available */}
